c0cd8fa7d9a76f34f557376628728dfa3373a00a
Konrad Pazgan <hello@konradpazgan.pl>
2021-11-17 13:27:21 +0100

    7th iteration: add loop until user is unhappy

--- dice_roller.rb
+++ dice_roller.rb
@@ -2,29 +2,33 @@ def roll(range=1..20)
   rand(range)
 end
 
-k30_roll1 = roll(1..30)
-if k30_roll1 >= 1 && k30_roll1 <= 7
-  @k30_roll2 = roll(1..30)
-end
-
 loop do
-  @rolls = []
-  7.times do
+  k30_roll1 = roll(1..30)
+  if k30_roll1 >= 1 && k30_roll1 <= 7
+    @k30_roll2 = roll(1..30)
+  end
+
+  loop do
+    @rolls = []
+    7.times do
+      @rolls << roll
+    end
+    if @k30_roll2 == 1
+      @rolls -= @rolls.min
+      @rolls -= @rolls.min
+    else
+      @rolls -= @rolls.minmax
+    end
+    break if @rolls.sum >= 55
+  end
+
+  if (24..30).include?(@k30_roll2)
+    @rolls << 25
+  else
     @rolls << roll
   end
-  if @k30_roll2 == 1
-    @rolls -= @rolls.min
-    @rolls -= @rolls.min
-  else
-    @rolls -= @rolls.minmax
-  end
-  break if @rolls.sum >= 55
-end
 
-if (24..30).include?(@k30_roll2)
-  @rolls << 25
-else
-  @rolls << roll
+  puts "Your randomly generated set: #{@rolls.join(" ")}. Are you happy?"
+  is_happy = gets.chomp
+  break if is_happy == 'YES!'
 end
-
-puts @rolls
