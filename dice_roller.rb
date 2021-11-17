f9a7c9bfb272d83a71b5be174929877a4f953100
Konrad Pazgan <hello@konradpazgan.pl>
2021-11-17 13:06:39 +0100

    5th iteration: add aditional two k30 rolls and change excluding behaviour depending on it

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,17 +1,26 @@
-def roll
-  rand(1..20)
+def roll(range=1..20)
+  rand(range)
 end
-rolls = []
+
+k30_roll1 = roll(1..30)
+if k30_roll1 >= 1 && k30_roll1 <= 7
+  @k30_roll2 = roll(1..30)
+end
 
 loop do
-  rolls = []
+  @rolls = []
   7.times do
-    rolls << roll
+    @rolls << roll
   end
-  rolls = rolls - rolls.minmax
-  break if rolls.sum >= 55
+  if @k30_roll2 == 1
+    @rolls -= @rolls.min
+    @rolls -= @rolls.min
+  else
+    @rolls -= @rolls.minmax
+  end
+  break if @rolls.sum >= 55
 end
 
-rolls << roll
+@rolls << roll
 
-puts rolls
+puts @rolls
