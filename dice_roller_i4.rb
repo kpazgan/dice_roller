5ba2179215d1d82a77ffc0a3510ff0bd2b952ca3
Konrad Pazgan <hello@konradpazgan.pl>
2021-11-18 09:46:59 +0100

    fix: fixed 4th iteration

---
+++ dice_roller_i4.rb
@@ -0,0 +1,19 @@
+def roll
+  rand(1..20)
+end
+
+
+loop do
+  @rolls = []
+  7.times do
+    @rolls.push(roll)
+  end
+
+  @rolls.delete_at(@rolls.index(@rolls.min))
+  @rolls.delete_at(@rolls.index(@rolls.max))
+  break if @rolls.sum >= 55
+end
+
+@rolls.push(roll)
+
+puts @rolls
